2a312e07e860ae50b95f0ad761e2926021527020
Alex Fedoseev <alex.fedoseev@gmail.com>
2018-03-10 17:14:58 +0800

    Add interface generator

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,22 @@ import * as cp from 'child_process'
 import * as os from 'os'
 import { AutoLanguageClient, ActiveServer } from 'atom-languageclient'
 import * as path from 'path'
-import { CompositeDisposable, FilesystemChangeEvent } from 'atom';
+import { CompositeDisposable, FilesystemChangeEvent } from 'atom'
 import * as languageServer from 'ocaml-language-server'
 import * as fs from 'fs-extra'
 import merge from 'deepmerge'
 import * as pkg from '../package.json'
 import config from './config'
 import * as Utils from './utils'
-import { DeepPartial, Config } from './types'
+import { DeepPartial, Config, FileExtension } from './types'
 
 const confFile = ".atom/ide-reason.json"
-const defaultConfig = languageServer.ISettings.defaults.reason
+const defaultConfig = merge(
+  {},
+  languageServer.ISettings.defaults.reason,
+  // @ts-ignore: `bsc` doesn't exist in languageServer.ISettings
+  { path: { bsc: 'bsc' } },
+)
 
 const scopes = [
   'source.reason',
@@ -62,7 +67,7 @@ class ReasonMLLanguageClient extends AutoLanguageClient {
         console.log('All dependencies installed, good to go')
       })
 
-    this.subscriptions = new CompositeDisposable();
+    this.subscriptions = new CompositeDisposable()
 
     this.subscriptions.add(
       atom.commands.add('atom-workspace', {
@@ -74,9 +79,38 @@ class ReasonMLLanguageClient extends AutoLanguageClient {
     )
 
     this.subscriptions.add(
+      atom.commands.add('atom-text-editor[data-grammar="source reason"]', {
+        [`${pkg.name}:generate-interface`]: () => this.generateInterfaceFromEditor('re'),
+      }),
+      atom.commands.add('atom-text-editor[data-grammar="source ocaml"]', {
+        [`${pkg.name}:generate-interface`]: () => this.generateInterfaceFromEditor('ml'),
+      }),
+      atom.commands.add(".tree-view .file .name[data-name$=\\.re]", {
+        [`${pkg.name}:generate-interface`]: event => this.generateInterfaceFromTreeView(event, 're'),
+      }),
+      atom.commands.add(".tree-view .file .name[data-name$=\\.ml]", {
+        [`${pkg.name}:generate-interface`]: event => this.generateInterfaceFromTreeView(event, 'ml'),
+      }),
+    )
+
+    this.subscriptions.add(
       atom.notifications.onDidAddNotification(notification => this.autoDismissBrokenPipeError(notification)),
       atom.project.onDidChangeFiles(events => this.notifyServersOnProjectConfigChange(events)),
     )
+  }
+
+  showWarning(message: string, detail?: string) {
+    atom.notifications.addWarning(message, {
+      detail,
+      dismissable: true,
+    })
+  }
+
+  showError(message: string, error: Error) {
+    atom.notifications.addError(message, {
+      detail: error.stack ? error.stack.toString() : undefined,
+      dismissable: true,
+    })
   }
 
   autoDismissBrokenPipeError(notification: any) {
@@ -111,9 +145,7 @@ class ReasonMLLanguageClient extends AutoLanguageClient {
     }
     const confPath = path.join(curProject, confFile)
     if (fs.existsSync(confPath)) {
-      return atom.notifications.addWarning('ide-reason.json already exists!', {
-        description: confPath,
-      })
+      return this.showWarning('ide-reason.json already exists!', confPath)
     }
     await fs.outputFile(
       confPath,
@@ -131,8 +163,6 @@ class ReasonMLLanguageClient extends AutoLanguageClient {
 
   startServerProcess(projectPath: string) {
     const serverPath = require.resolve('ocaml-language-server/bin/server')
-
-
     const confFromFile: DeepPartial<Config> = Utils.readFileConf(path.join(projectPath, confFile)) || {}
     this.confPerProject = confFromFile
     const envPath = process.env.PATH +
@@ -154,6 +184,56 @@ class ReasonMLLanguageClient extends AutoLanguageClient {
       this.subscriptions.dispose()
     }
     return result
+  }
+
+  // Interface generator
+  generateInterfaceFromTreeView(event: any, ext: FileExtension) {
+    this.generateInterface(event.target.dataset.path, ext)
+  }
+
+  generateInterfaceFromEditor(ext: FileExtension) {
+    const activeEditor = atom.workspace.getActiveTextEditor()
+    if (!activeEditor) {
+      this.showWarning("No active editor found")
+      return
+    }
+    const srcAbsPath = activeEditor.getPath()
+    if (!srcAbsPath) {
+      this.showWarning(
+        "Can't generate interface file from unsaved buffer. Save source file first.",
+      )
+      return
+    }
+    this.generateInterface(srcAbsPath, ext)
+  }
+
+  generateInterface(srcAbsPath: string, ext: FileExtension) {
+    const [root, srcRelPath] = atom.project.relativizePath(srcAbsPath)
+    if (!root) {
+      this.showWarning("Can't find root directory of the project")
+      return
+    }
+    const baseRelPath = srcRelPath.substring(0, srcRelPath.length - 2)
+    const cmiAbsPath = path.join(root, "lib", "bs", baseRelPath + "cmi")
+    const interfaceAbsPath = path.join(root, baseRelPath + ext + "i")
+    const bscBin = atom.config.get(this.getRootConfigurationKey()).path.bsc
+    const cmd =
+      ext === 'ml'
+      ? `${bscBin} ${cmiAbsPath}`
+      : `${bscBin} -bs-re-out ${cmiAbsPath}`
+    cp.exec(cmd, (error, stdout) => {
+      if (error) {
+        this.showError("Oops! Can't generate interface file", error)
+        return
+      }
+      fs.outputFile(interfaceAbsPath, stdout, error => {
+        if (error) {
+          this.showError("Oops! Can't write generated interface file", error)
+          return
+        }
+        atom.workspace.open(interfaceAbsPath)
+      })
+    })
   }
 }
 
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,17 @@
-export { DeepPartial } from 'deepmerge'
 import { ISettings } from 'ocaml-language-server'
+export { DeepPartial } from 'deepmerge'
 
-export type Config = ISettings['reason']
+export type OLSConfig = ISettings['reason']
 
-export type ToolKeys = keyof Config["path"];
+export type Config = {
+  codelens: OLSConfig['codelens'];
+  debounce: OLSConfig['debounce'];
+  diagnostics: OLSConfig['diagnostics'];
+  path: OLSConfig['path'] & { bsc: string; };
+  format: OLSConfig['format'];
+  server: OLSConfig['server'];
+}
+
+export type ToolKeys = keyof Config['path'];
+
+export type FileExtension = 're' | 'ml';
